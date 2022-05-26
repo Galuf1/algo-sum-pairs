5306ee45f2b3bdc5ae3840a4833698f367d83ece
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-26 21:12:21 +0200

    solved

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,10 @@
-def sum_pairs():
+def sum_pairs(array, number):
+    ans = []
+    for i in array:
+        for j in array:
+            if i + j == number and [j,i] not in ans:
+                ans.append([i,j])
+    if len(ans) == 0:
+        return 'unable to find pairs'
+    return ans
+
